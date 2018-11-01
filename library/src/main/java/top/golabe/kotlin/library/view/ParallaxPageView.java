2713629e089a99f523be5770a9cdd62bb2565a2a
yuequan <2643552374@qq.com>
2018-11-01 18:29:38 +0800

    调整

--- library/src/main/java/top/golabe/kotlin/library/view/ParallaxPageView.java
+++ library/src/main/java/top/golabe/kotlin/library/view/ParallaxPageView.java
@@ -53,69 +53,25 @@ public class ParallaxPageView extends ViewPager {
         @Override
         public void onPageScrolled(int position, float positionOffset,
                                    int positionOffsetPixels) {
-
-
             containerWidth=getWidth();
 
-            //在翻页的过程中，不断根据视图的标签中对应的动画参数，改变视图的位置或者透明度
-            //获取到进入的页面
             ParallaxFragment inFragment = null;
             try {
                 inFragment = mFragments.get(position - 1);
-            } catch (Exception e) {}
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-            //获取到退出的页面
             ParallaxFragment outFragment = null;
             try {
                 outFragment = mFragments.get(position);
-            } catch (Exception e) {}
-
-            if (inFragment != null) {
-                //获取Fragment上所有的视图，实现动画效果
-
-                List<View> inViews = inFragment.getParallaxViews();
-                if (inViews != null) {
-                    for (View view : inViews) {
-                         if (!(view instanceof  ParallaxPageContainer)){
-                             continue;
-                         }
-
-                        for (int i = 0; i < ((ParallaxPageContainer) view).getChildCount(); i++) {
-                            View childView = ((ParallaxPageContainer) view).getChildAt(i);
-                            IWrapContainer container = (IWrapContainer) childView;
-                            if (container!=null){
-                                container.onTranslationInY(containerWidth - positionOffsetPixels);
-                                container.onTranslationInX(containerWidth - positionOffsetPixels);
-                                container.onAlphaIn(containerWidth-positionOffsetPixels);
-                            }
-                        }
-
-                    }
-                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-            if(outFragment != null){
+            inFragment(positionOffsetPixels, inFragment);
 
-                List<View >outViews = outFragment.getParallaxViews();
-                if (outViews != null) {
-                    for (View view : outViews) {
-                        if (!(view instanceof  ParallaxPageContainer)){
-                            continue;
-                        }
-
-                        for (int i = 0; i < ((ParallaxPageContainer) view).getChildCount(); i++) {
-                            View childView = ((ParallaxPageContainer) view).getChildAt(i);
-                            IWrapContainer container = (IWrapContainer) childView;
-                            if (container!=null){
-                                container.onTranslationOutY(0 - positionOffsetPixels);
-                                container.onTranslationOutX(0 - positionOffsetPixels);
-                                container.onAlphaOut(0-positionOffsetPixels);
-                            }
-                        }
-
-                    }
-                }
-            }
+            outFragment(positionOffsetPixels, outFragment);
 
         }
 
@@ -129,6 +85,55 @@ public class ParallaxPageView extends ViewPager {
 
         }
     };
+
+    private void outFragment(int positionOffsetPixels, ParallaxFragment outFragment) {
+        if(outFragment != null){
+
+            List<View > outViews = outFragment.getParallaxViews();
+            if (outViews != null) {
+                for (View view : outViews) {
+                    if (!(view instanceof ParallaxPageContainer)){
+                        continue;
+                    }
+
+                    for (int i = 0; i < ((ParallaxPageContainer) view).getChildCount(); i++) {
+                        View childView = ((ParallaxPageContainer) view).getChildAt(i);
+                        IWrapContainer container = (IWrapContainer) childView;
+                        if (container!=null){
+                            container.onTranslationOutY(0 - positionOffsetPixels);
+                            container.onTranslationOutX(0 - positionOffsetPixels);
+                            container.onAlphaOut(0-positionOffsetPixels);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void inFragment(int positionOffsetPixels, ParallaxFragment inFragment) {
+        if (inFragment != null) {
+            List<View> inViews = inFragment.getParallaxViews();
+            if (inViews != null) {
+                for (View view : inViews) {
+                     if (!(view instanceof ParallaxPageContainer)){
+                         continue;
+                     }
+
+                    for (int i = 0; i < ((ParallaxPageContainer) view).getChildCount(); i++) {
+                        View childView = ((ParallaxPageContainer) view).getChildAt(i);
+                        IWrapContainer container = (IWrapContainer) childView;
+                        if (container!=null){
+                            container.onTranslationInY(containerWidth - positionOffsetPixels);
+                            container.onTranslationInX(containerWidth - positionOffsetPixels);
+                            container.onAlphaIn(containerWidth-positionOffsetPixels);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 
 
 }
